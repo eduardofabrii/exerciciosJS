fd51e02055f16be269372deafbc47920b9cd2076
Eduardo Fabri <eduardohfabri@gmail.com>
2024-06-19 11:09:20 -0300

    Aula 05

---
+++ exercicios/aula03/ex005.js
@@ -0,0 +1,15 @@
+
+
+var velocidade = 65
+
+
+console.log(`A velocidade do seu carro é ${velocidade} km/h.`)
+
+
+
+if (velocidade > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+else {
+    console.log(`Velocidade ideal. Dirija sempre usando cinto de segurança!`)
+}
